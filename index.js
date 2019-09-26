899c72c5dba7f67c0dce466922f1f47cdb269328
Fabio Papa <fabtheman@gmail.com>
2019-09-26 15:04:54 -0700

    Add ability to make an API call to get the user's IP address

---
+++ index.js
@@ -0,0 +1,1 @@
+const { fetchMyIP } = require('./iss.js');
---
+++ iss.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = (callback) => {
+  const API_URL = 'https://api.ipify.org?format=json';
+  request(API_URL, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    callback(null, JSON.parse(body).ip);
+  });
+};
+
+module.exports = { fetchMyIP };
